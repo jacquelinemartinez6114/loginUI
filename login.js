eccde311d5961c94506f7e9b9e6ae8a1dba76d4b
Jacqueline M <jacqueline.martinez6114@gmail.com>
2023-08-28 21:32:22 -0700

    feat(html and js)add javascript file and modifty html

---
+++ login.js
@@ -0,0 +1,15 @@
+let attempt=3; //number of login attempts permitted
+
+//function that runs when user clicks submit button with strict rules
+function validate(){
+    let username= document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+        if (username == "jacqueline" && password == "1234") {
+            alert ("Login Successful");
+            return false;
+        }
+        else {
+            attempt --; /*Decrement by one */
+            alert("You have left "+attempt+" attempt;");
+        }
+}
